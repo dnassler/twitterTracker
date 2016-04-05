567a31436f64fab67eed2a277c4717f402bda472
Derek Nassler <dnassler@rogers.com>
2016-04-05 11:25:31 -0400

    removed config from comments

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var port = process.env.PORT || 8081;
 var io = require('socket.io')(port);
 // var cfg0 = require('./config.json');
-// {
-//     "consumer_key": "QOQJimqeQNW30avA0eX3zg4FR",
-//     "consumer_secret": "BPuOvOvi2AbIiJx8i0YmtcQFgPJOTGGb70GDPHyCysHtdCo9IL",
-//     "token": "20842155-FEcaE2xF93mCKkCkWm9TMJ11P93BTc7EV92Pe77GB",
-//     "token_secret": "UmPLOFELAEhEEoqyfgNrpb1A7SnVZ6FpyabgDKbTyJBXw"
-// }
 var cfg = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
